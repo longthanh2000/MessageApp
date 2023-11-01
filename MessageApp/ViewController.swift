b270e3f8bf048dd165390a63cc1a55936c1d4856
longNT <longnguyenthanh496@gmail.com>
2023-11-01 08:15:51 +0700

    Initial Commit

---
+++ MessageApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessageApp
+//
+//  Created by long Bu03 on 01/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
